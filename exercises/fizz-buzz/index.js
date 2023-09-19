8d9cc282eb4291b1ced5e2dc5e6220812f612766
Filipe Moreira <filipesantos.py@gmail.com>
2023-09-19 11:12:22 -0300

    adicionando um loop

--- exercises/fizz-buzz/index.js
+++ exercises/fizz-buzz/index.js
@@ -1,21 +1,26 @@
 var output = []
 var number = 1
 
-function fizzBuzz(){    
-    if (number % 5 === 0 && number % 3 === 0){
-        output.push("FizzBuzz")
-    }else if (number % 3 === 0){
-        output.push("Fizz")
-    }else if (number % 5 === 0){
-        output.push("Buzz")
-    }else {
-        output.push(number)
+function fizzBuzz(){
+
+    while (number <= 100 ){
         
-    }
-
-    number++
+        if (number % 5 === 0 && number % 3 === 0){
+            output.push("FizzBuzz")
+        }else if (number % 3 === 0){
+            output.push("Fizz")
+        }else if (number % 5 === 0){
+            output.push("Buzz")
+        }else {
+            output.push(number)
+        }
+    
+        number++
+        }
+    
     console.log(output)
-
+    
 }
+
 
 fizzBuzz()
