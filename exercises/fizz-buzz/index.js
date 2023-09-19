54d2545d44fdce29acea74c35b55803a4205de8d
Filipe Moreira <filipesantos.py@gmail.com>
2023-09-19 10:31:05 -0300

    alteração na incrementação

--- exercises/fizz-buzz/index.js
+++ exercises/fizz-buzz/index.js
@@ -4,19 +4,18 @@ var number = 1
 function fizzBuzz(){    
     if (number % 5 === 0 && number % 3 === 0){
         output.push("FizzBuzz")
-        number++
     }else if (number % 3 === 0){
         output.push("Fizz")
-        number++
     }else if (number % 5 === 0){
         output.push("Buzz")
-        number++
     }else {
         output.push(number)
-        number++
+        
     }
+
+    number++
     console.log(output)
-    
+
 }
 
 fizzBuzz()
